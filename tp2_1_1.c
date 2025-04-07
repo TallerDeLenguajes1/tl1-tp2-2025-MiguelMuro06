5fc333ce3cf16fa423372f5b9cec0712a03a553a
Miguel Muro <muromiguel17@gmail.com>
2025-04-07 01:21:02 -0300

    inicial

---
+++ tp2_1_1.c
@@ -0,0 +1,17 @@
+// codigo a completar
+#include<stdio.h>
+#include<time.h>
+#include<stdlib.h>
+#define N 20
+int main (){
+
+int i;
+double vt[N];
+srand(time(NULL));
+for(i = 0;i<N; i++)
+{
+vt[i]=1+rand()%100;
+printf("\n el numero es = %2.f", vt[i]);
+}
+return 0;
+}
